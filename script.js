2dbbdb488893a8f6169dd1d7aa003e0a0fc5d362
BasitTech1 <abdulbasitpiralizai@gmail.com>
2025-08-30 17:07:10 +0500

    sevebth page created

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleBtn = document.getElementById('toggle-btn')
+const CLickList = document.getElementById('CLick-list')
+
+CLickList.addEventListener('click', function(){
+    toggleBtn.style.opacity = 100
+})
