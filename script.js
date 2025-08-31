ea155834832f9f082fb406eb7301591320d6d6e0
BasitTech1 <abdulbasitpiralizai@gmail.com>
2025-08-31 15:11:02 +0500

    initial comment

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 const clickList = document.getElementById('CLick-list');
 const toggleBtn = document.getElementById('toggle-btn');
 
-clickList.addEventListener('click', function (e) {
-    e.stopPropagation();
-    toggleBtn.classList.toggle('visible');
-});
-
-document.addEventListener('click', function (e) {
-    if (!clickList.contains(e.target) && !toggleBtn.contains(e.target)) {
-        toggleBtn.classList.remove('visible');
-    }
-});
-
-toggleBtn.addEventListener('click', function (e) {
-    e.stopPropagation();
-});
+clickList.addEventListener('click', function(){
+    toggleBtn.classList.toggle('visible')
+})
