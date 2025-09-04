bab19876196944f81a757a583c2aa835a4d53e3e
BasitTech1 <abdulbasitpiralizai@gmail.com>
2025-09-04 17:16:03 +0500

    initial comment

--- script.js
+++ script.js
@@ -72,28 +72,3 @@ function hideBox2() {
 }
 
 
-// ----------------------------------trainer-slider-------------------------
-
-let scrollContainer = document.querySelector(".trainer-slider")
-let backBtn = document.getElementById("backBtn")
-let nextBtn = document.getElementById("nextBtn")
-
-scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY
-    scrollContainer.style.scrollBehavior = 'auto'
-})
-
-function BackButtonClick() {
-    backBtn.addEventListener("click", () => {
-        scrollContainer.style.scrollBehavior = 'smooth'
-        scrollContainer.scrollLeft -= 900
-    })
-}
-
-function nextButtonClick() {
-    nextBtn.addEventListener("click", () => {
-        scrollContainer.scrollLeft += 900
-        scrollContainer.style.scrollBehavior = 'smooth'
-    })
-}
