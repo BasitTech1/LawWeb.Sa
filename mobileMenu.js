53e1a5a050d5d340fcc2e9bb2a71816e22bceda9
BasitTech1 <abdulbasitpiralizai@gmail.com>
2025-10-21 12:32:51 +0500

    initial comment

--- mobileMenu.js
+++ mobileMenu.js
@@ -2,9 +2,19 @@
 
 document.addEventListener('DOMContentLoaded', function () {
     const menuToggle = document.getElementById("menu-toggle")
+    const menuCross = document.getElementById("menu-cross")
     const menuBtns = document.getElementById("menu-btns")
 
     menuToggle.addEventListener('click', function () {
         menuBtns.style.opacity = '100'
+        menuCross.style.opacity = '1'
+        menuToggle.style.opacity = '0'
+    })
+
+    menuCross.addEventListener('click', function () {
+        menuBtns.style.opacity = '0'
+        menuCross.style.opacity = '0'
+        menuToggle.style.opacity = '1'
+        console.log('click')   
     })
 })
